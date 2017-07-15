878f9e6e9aaca7fa85ac0769af14d460a9fa0663
Terra Ashley <ash@ragingskies.net>
2017-07-14 22:29:09 -0700

    Added SendGrid SMTP settings to development and production environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,25 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
+  # We care very much if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "sendgrid.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
